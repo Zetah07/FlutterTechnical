1e11a409fef39d7b08c130f22ca3a6089d048491
Zetah07 <sebastiancastro0792@gmail.com>
2023-07-18 14:09:24 -0500

    app theme, and home screen

---
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+const colorList = <Color> [
+  Colors.black87,
+  Colors.black,
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.yellow,
+  Colors.purple,
+  Colors.orange,
+  Colors.pink,
+  Colors.teal,
+  Colors.cyan,
+  Colors.brown,
+  Colors.grey,
+  Colors.blueGrey,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+    }):assert(selectedColor >= 0, 'Selected color must be greater than 0'),
+      assert(selectedColor < colorList.length, 'Selected color must be less than ${colorList.length}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+    )
+  );
+}
