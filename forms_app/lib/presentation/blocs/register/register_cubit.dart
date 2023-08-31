ed93831ad4723a23aed904d51b21536ffb427a47
Zetah07 <sebastiancastro0792@gmail.com>
2023-08-30 23:49:40 -0500

    conection cubit with form

--- forms_app/lib/presentation/blocs/register/register_cubit.dart
+++ forms_app/lib/presentation/blocs/register/register_cubit.dart
@@ -1,8 +1,25 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'register_state.dart';
 
-class RegisterCubit extends Cubit<RegisterState> {
-  RegisterCubit() : super(RegisterInitial());
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(const RegisterFormState());
+
+  void onSubmit () {
+    emit(state.copyWith(formStatus: FormStatus.posting));
+  }
+
+  void usernameChanged(String value) {
+    emit(state.copyWith(username: value));
+  }
+
+  void emailChanged(String value) {
+    emit(state.copyWith(email: value));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(password: value));
+  }
+
 }
--- forms_app/lib/presentation/blocs/register/register_state.dart
+++ forms_app/lib/presentation/blocs/register/register_state.dart
@@ -1,10 +1,36 @@
 part of 'register_cubit.dart';
 
-sealed class RegisterState extends Equatable {
-  const RegisterState();
 
-  @override
-  List<Object> get props => [];
-}
+enum FormStatus { invalid, valid, validating, posting }
 
-final class RegisterInitial extends RegisterState {}
+
+class RegisterFormState extends Equatable {
+  
+  final FormStatus formStatus;
+  final String username;
+  final String email;
+  final String password;
+
+  const RegisterFormState({
+    this.formStatus = FormStatus.invalid,
+    this.username = '',
+    this.email = '',
+    this.password = '',
+  });
+
+
+RegisterFormState copyWith({
+  FormStatus? formStatus,
+  String? username,
+  String? email,
+  String? password,
+}) => RegisterFormState(
+  formStatus: formStatus ?? this.formStatus,
+  username: username ?? this.username,
+  email: email ?? this.email,
+  password: password ?? this.password,
+);
+  
+    @override
+    List<Object?> get props => [formStatus, username, email, password];
+  }
